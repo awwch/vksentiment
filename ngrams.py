e2b5d60316e52880ab0fc45d540878b3c771b95b
awwch <drolls4ever@gmail.com>
2017-06-09 12:04:38 +0300

    ngrams

---
+++ ngrams.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from collections import Counter
+#from nltk.stem import SnowballStemmer
+from nltk import ngrams
+import string
+import re
+from mystopwords import stopWords
+
+#st = SnowballStemmer("russian")
+
+def ngramize(text,n):
+    n_grams= []
+    text = text.split()
+    stop_words = stopWords()    
+    for word in text:
+        fine_word = word.strip(string.punctuation)
+        fine_word = re.sub("[^а-яА-ЯЁё]","", fine_word)
+        text.insert(text.index(word),fine_word)
+        text.remove(word)
+        if fine_word in stop_words or fine_word == '':
+            text.remove(fine_word)
+    n_grams += ngrams(text,n)
+    return n_grams
+
+table = [] 
+n = 2
+f = open('all '+str(n)+'-grams.txt','a',encoding = 'utf-8')         
+for i in range(14):
+    name = 'bns_ru\\comments'+str(i)+'.csv'
+    try:
+        table = pd.read_csv(name, header=0, \
+                    delimiter=';', quoting=3)
+        print(len(table))
+        for j in range(len(table)):
+            gram = ngramize(str(table['text'][j]),n)
+            for g in gram:
+                f.write(str(g)+'\n')
+            print(len(table))
+            print(j)
+    except FileNotFoundError:
+        continue
+f.close()
+
+with open('2.txt','r',encoding = 'utf-8') as f:
+    f = f.readlines()
+freq_ngrams = Counter(f).most_common()
+
+with open('common 2-grams.txt','w',encoding = 'utf-8') as f:
+    for line in freq_ngrams:
+        f.write(str(line)+'\n')
+
+
+#import pymorphy2
+#morph = pymorphy2.MorphAnalyzer()
+#for element in freq_ngrams:
+ #   for word in element[0]:
+  #      word = re.sub('[^А-яЁ]',' ',word)
+   #     p = morph.parse(word)[0]
+    #    gram_info = str(p.tag)
+     #   if 'PREP' in gram_info or 'CONJ'in gram_info or 'NPRO' in gram_info or word == '':
+      #      try:
+       #         freq_ngrams.remove(element)
+        #    except ValueError:
+         #       continue
+            
